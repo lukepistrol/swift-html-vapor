50633c481cd937f082bfb145390f455b198fa305
Lukas Pistrol <l.pistrol@gmail.com>
2023-05-23 01:44:14 +0200

    add async/await support

--- Sources/HtmlVaporSupport/HtmlVaporSupport.swift
+++ Sources/HtmlVaporSupport/HtmlVaporSupport.swift
@@ -2,13 +2,22 @@
 import Vapor
 
 extension Html.Node: ResponseEncodable {
-  public func encodeResponse(for request: Request) -> EventLoopFuture<Response> {
-    var headers = HTTPHeaders()
-    headers.add(name: .contentType, value: "text/html; charset=utf-8")
+    public func encodeResponse(for request: Request) -> EventLoopFuture<Response> {
+        var headers = HTTPHeaders()
+        headers.add(name: .contentType, value: "text/html; charset=utf-8")
 
-    return request.eventLoop.makeSucceededFuture(.init(
-      headers: headers,
-      body: .init(string: Html.render(self))
-    ))
-  }
+        return request.eventLoop.makeSucceededFuture(.init(
+            headers: headers,
+            body: .init(string: Html.render(self))
+        ))
+    }
 }
+
+
+#if compiler(>=5.5) && canImport(_Concurrency)
+extension Html.Node: AsyncResponseEncodable {
+    public func encodeResponse(for request: Request) async throws -> Response {
+        try await encodeResponse(for: request).get()
+    }
+}
+#endif
